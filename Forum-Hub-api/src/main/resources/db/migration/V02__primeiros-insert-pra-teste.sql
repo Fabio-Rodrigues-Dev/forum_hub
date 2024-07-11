6efe8929d1adbd0cb83b1d7d379dcb8326776f9b
Fabio-Rodrigues-Dev <158431227+Fabio-Rodrigues-Dev@users.noreply.github.com>
2024-07-11 02:57:59 -0300

    Add files via upload

---
+++ Forum-Hub-api/src/main/resources/db/migration/V02__primeiros-insert-pra-teste.sql
@@ -0,0 +1,11 @@
+insert into perfil (id, nome)
+values(1, 'ALUNO');
+
+insert into usuario (id, nome, email, senha, perfil_id)
+values(1, 'teste', 'teste@email.com', 'B#11112222', 1);
+
+insert into usuario (id, nome, email, senha, perfil_id)
+values(2, 'teste2', 'teste2@email.com', 'A%12345678', 1);
+
+insert curso (id, nome, categoria)
+values(1, 'Java', 'Programação');
